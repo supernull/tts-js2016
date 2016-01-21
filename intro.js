3b42892d77a6da52a47a5f41a0d14ba69dc85626
aj <aj@opuscraft.com>
2016-01-21 13:28:06 -0500

    First commit

---
+++ intro.js
@@ -0,0 +1,17 @@
+//
+// var cupsConsumed = 1;
+//
+// if(cupsConsumed>=3){
+//     console.log("I think I'm okay for now!");
+//   } else {
+//    console.log("Yes I think I'll take another cup.");
+// }
+
+// var x = 99
+//
+// for(; x>1; x-- ){
+//   console.log(x + " bottles of beer on the wall");
+// }
+//
+// console.log("1 bottle of beer on the wall!")
+// console.log("Hey, we need more beer!")
